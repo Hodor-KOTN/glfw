386a53f2eaaa46ad4c02cc79377f746f5ac6f720
Ciprian Gavrilovici <ciprian.gavrilovici@gmail.com>
2020-08-28 05:24:42 -0700

    added premake settings for linux - x11 compilation with CodeLite

--- premake5.lua
+++ premake5.lua
@@ -22,7 +22,6 @@ project "GLFW"
         --buildoptions { "-std=c11", "-lgdi32" }
         systemversion "latest"
         staticruntime "On"
-        
         files
         {
             "src/win32_init.c",
@@ -32,14 +31,31 @@ project "GLFW"
             "src/win32_thread.c",
             "src/win32_window.c",
             "src/wgl_context.c",
-            "src/egl_context.c",
-            "src/osmesa_context.c"
+            "src/egl_context.c"
         }
-
 		defines 
 		{ 
             "_GLFW_WIN32",
             "_CRT_SECURE_NO_WARNINGS"
+		}
+	filter "system:linux"
+		systemversion "latest"
+		staticruntime "On"
+		files
+		{
+			"src/x11_init.c",
+			"src/linux_joystick.c",
+			"src/x11_monitor.c",
+			"src/posix_time.c",
+			"src/posix_thread.c",
+			"src/xkb_unicode.c",
+			"src/x11_window.c",
+			"src/glx_context.c",
+	        "src/egl_context.c"
+		}
+		defines
+		{
+			"_GLFW_X11"
 		}
 
 	filter "configurations:Debug"
